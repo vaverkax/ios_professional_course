0b6d87e05eb64b6f2ae127b7461ec1b217434011
Виктор Билин <13656832+vaverkax@users.noreply.github.com>
2022-02-24 20:27:36 +0300

    Initial Commit

---
+++ ios_professional_course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_professional_course
+//
+//  Created by Владимир Скоробогатько on 24.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_professional_course/ios_professional_courseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_professional_courseApp.swift
+//  ios_professional_course
+//
+//  Created by Владимир Скоробогатько on 24.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ios_professional_courseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
